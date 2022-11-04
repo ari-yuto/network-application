41bb8c8cd232267863e3871de4cb05d49d03a101
ari-yuto <115969520+ari-yuto@users.noreply.github.com>
2022-11-05 00:07:26 +0900

    Add files via upload
    星2まで完成

--- Paint2.java
+++ Paint2.java
@@ -10,71 +10,60 @@ import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 
 public class Paint2 extends Frame implements MouseListener, MouseMotionListener {
-	
-	static int _MAX_DRAW = 30;
-	
-	int x,y;
-	ArrayList<Figure> objList;
-	Figure obj;
-	
-	public static void main(String[] args) {
-		if(args.length !=0) _MAX_DRAW = Integer.parseInt(args[0]);
-		Paint2 f = new Paint2();
-		f.setSize(640,480);
-		f.setTitle("Print Sample");
-		f.addWindowListener(new WindowAdapter() {
-		@Override public void windowClosing(WindowEvent e) {
-			System.exit(0);
-		}});
-		f.setVisible(true);
-	}
-	
-	public Paint2() {
-		objList = new ArrayList<Figure>();
-		addMouseListener(this);
-		addMouseMotionListener(this);
-	}
-	
-	@Override public void paint(Graphics g) {
-		Figure f;
-		for (int i = 0; i < objList.size(); i++) {
-			f = objList.get(i);
-			f.paint(g);
-		}
-		if(obj != null) obj.paint(g);
-	}
-	
-	@Override public void mousePressed(MouseEvent e) {
-		x = e.getX();
-		y = e.getY();
-		int list_count = objList.size() + 1;
-		if(list_count > _MAX_DRAW) {
-			objList.remove(0);
-			list_count--;
-		}
-		obj = new Circle();
-		System.out.println(list_count);
-		obj.moveto(x, y);
-		repaint();
-	}
-	
-	@Override public void mouseReleased(MouseEvent e) {
-		x = e.getX();
-		y = e.getY();
-		obj.moveto(x, y);
-		objList.add(obj);
-		obj = null;
-		repaint();
-	}
-	@Override public void mouseClicked(MouseEvent e) {}
-	@Override public void mouseEntered(MouseEvent e) {}
-	@Override public void mouseExited(MouseEvent e) {}
-	@Override public void mouseDragged(MouseEvent e) {
-		x = e.getX();
-		y = e.getY();
-		if(obj != null)	obj.moveto(x, y); 		
-		repaint();
-	}
-	@Override public void mouseMoved(MouseEvent e) {}
+    int x, y,count;
+    ArrayList<Figure> objList;
+    Figure obj;
+    public static void main(String[] args) {
+        Paint2 f = new Paint2();
+        f.setSize(640, 480);
+        f.setTitle("Paint Sample");
+        f.addWindowListener(new WindowAdapter() {
+            @Override public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }});
+        f.setVisible(true);
+    }
+    Paint2() {
+        objList=new ArrayList<Figure>();
+        //マウス処理の登録
+        addMouseListener(this);
+        addMouseMotionListener(this);
+    }
+    @Override public void paint(Graphics g) {
+        Figure f;
+        for(int i=0;i<objList.size();i++) {
+            f=objList.get(i);
+            f.paint(g);
+        }
+        if (obj != null) obj.paint(g);
+    }
+    public void mousePressed(MouseEvent e) {
+        x = e.getX();
+        y = e.getY();
+        obj=new Circle();
+        obj.moveto(x,y);
+        repaint();
+        System.out.println(objList.size()+1);
+        if(objList.size()>=30) {
+        	objList.set(objList.size()%30, obj);
+        }
+    }
+    public void mouseReleased(MouseEvent e) {
+        x=e.getX();
+        y=e.getY();
+        obj.moveto(x,y);
+        objList.add(obj);
+        obj=null;
+        repaint();
+    }
+    public void mouseClicked(MouseEvent e) {}
+    public void mouseEntered(MouseEvent e) {}
+    public void mouseExited(MouseEvent e) {}
+    public void mouseDragged(MouseEvent e) {
+        x = e.getX();
+        y = e.getY();
+        if(obj !=null)obj.moveto(x,y);
+        repaint();
+    }
+    public void mouseMoved(MouseEvent e) {}
 }
-
